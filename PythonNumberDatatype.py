ba1905c686efaad6bf47f4be21a450599086f946
jmartinez419 <justynmartinez419@gmail.com>
2024-10-07 10:36:10 -0400

    first commit

---
+++ PythonNumberDatatype.py
@@ -0,0 +1,34 @@
+num1 = 10
+num2 = 20
+print(num1 + num2)
+
+num1 = 2.5
+num2 = 2.5
+print(num1 + num2)
+
+num1 = 5
+num2 = 2.0
+sum = num1 + num2
+print(float(sum))
+
+num1 = 10
+num2 = 5
+sum = num1 / num2
+print(sum)
+
+num1 = 20.0
+num2 = 10.0
+sum = num1 / num2
+print(sum)
+
+coffee = 10.00
+cappuccino = 20.00
+donut = 15.00
+subtotal = coffee * 3 + cappuccino * 4 + donut * 2
+sales_tax = subtotal * 0.8
+totalSale = subtotal + sales_tax
+print(totalSale)
+
+
+area = 3.14 * 64**2
+print(area)
